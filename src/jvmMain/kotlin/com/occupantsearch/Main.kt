97dfd41de140ed6c43bd5cf2ecd2041acd489410
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-06-27 18:47:52 +0100

    Use extension function

--- src/jvmMain/kotlin/com/occupantsearch/Main.kt
+++ src/jvmMain/kotlin/com/occupantsearch/Main.kt
@@ -1,12 +1,12 @@
 package com.occupantsearch
 
+import com.occupantsearch.koin.get
 import com.occupantsearch.koin.initKoin
 import com.occupantsearch.server.startServer
 import com.occupantsearch.update.UpdateController
-import org.koin.java.KoinJavaComponent.getKoin
 
 fun main() {
     initKoin()
-    getKoin().get<UpdateController>().start()
+    get<UpdateController>().start()
     startServer()
 }
