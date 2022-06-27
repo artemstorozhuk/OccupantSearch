2ba81745bc2a5d482a1c14a120edca8073efa2ae
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-06-27 20:37:13 +0100

    Introduce TopBar

--- src/jsMain/kotlin/OccupantsView.kt
+++ src/jsMain/kotlin/OccupantsView.kt
@@ -1,27 +1,16 @@
 import com.occupantsearch.occupant.Occupant
-import csstype.AlignItems
 import csstype.Display
 import csstype.FlexWrap
 import csstype.JustifyContent
-import csstype.Position
-import csstype.integer
-import csstype.pct
 import csstype.px
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import mui.icons.material.Search
-import mui.material.IconButton
-import mui.material.Input
-import mui.material.Paper
-import mui.system.sx
-import org.w3c.dom.HTMLInputElement
 import react.FC
 import react.Props
 import react.css.css
 import react.dom.html.ReactHTML.div
-import react.dom.html.ReactHTML.form
 import react.useEffectOnce
 import react.useState
 
@@ -40,43 +29,21 @@ val OccupantsView = FC<OccupantsListProps> { props ->
     var occupants by useState(props.occupants)
     var page by useState(props.page)
     var lastPage by useState(props.lastPage)
+
     useEffectOnce {
         scope.launch {
             occupants = client.getOccupants("", 0)
         }
     }
-    Paper {
-        css {
-            width = 100.pct
-            position = Position.fixed
-            zIndex = integer(100)
-        }
-        component = form
-        sx {
-            display = Display.flex
-            alignItems = AlignItems.center
-            width = 100.pct
-        }
-        Input {
-            css {
-                marginLeft = 10.px
-                width = 100.pct
-            }
-            placeholder = "Search"
-            value = name
-            onChange = { event ->
-                val value = (event.target as HTMLInputElement).value
-                scope.launch {
-                    page = 0
-                    query = value
-                    lastPage = false
-                    occupants = client.getOccupants(value, page)
-                    window.scrollTo(0.0, 0.0)
-                }
-            }
-        }
-        IconButton {
-            Search {
+
+    TopBar {
+        onSearch = { value ->
+            scope.launch {
+                page = 0
+                query = value
+                lastPage = false
+                occupants = client.getOccupants(value, page)
+                window.scrollTo(0.0, 0.0)
             }
         }
     }
@@ -92,6 +59,7 @@ val OccupantsView = FC<OccupantsListProps> { props ->
             OccupantCard { occupant = it }
         }
     }
+
     window.onscroll = {
         if (!lastPage && isInBottom()) {
             scope.launch {
---
+++ src/jsMain/kotlin/TopBar.kt
@@ -0,0 +1,60 @@
+import csstype.Position
+import csstype.integer
+import csstype.number
+import csstype.pct
+import csstype.px
+import mui.icons.material.Menu
+import mui.icons.material.Search
+import mui.material.AppBar
+import mui.material.AppBarPosition
+import mui.material.Box
+import mui.material.IconButton
+import mui.material.Input
+import mui.material.Size
+import mui.material.Toolbar
+import mui.system.sx
+import org.w3c.dom.HTMLInputElement
+import react.FC
+import react.Props
+import react.css.css
+
+external interface TopBarProps : Props {
+    var onSearch: (String) -> Unit
+}
+
+val TopBar = FC<TopBarProps> { props ->
+    Box {
+        sx {
+            flexGrow = number(1.0)
+        }
+        css {
+            width = 100.pct
+            position = Position.fixed
+            zIndex = integer(100)
+            margin = (-10).px
+        }
+        AppBar {
+            position = AppBarPosition.sticky
+            Toolbar {
+                IconButton {
+                    size = Size.large
+                    Menu {
+                    }
+                }
+                Input {
+                    css {
+                        width = 100.pct
+                    }
+                    placeholder = "Search…"
+                    onChange = { event ->
+                        props.onSearch((event.target as HTMLInputElement).value)
+                    }
+                }
+                IconButton {
+                    Search {
+                    }
+                }
+            }
+        }
+    }
+}
