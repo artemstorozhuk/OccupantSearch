62c94aebe88746a1187787edc345e65fd3b310f8
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-01 20:20:37 +0100

    Fix refresh chart bug

--- src/jsMain/kotlin/com/occupantsearch/ui/AnalyticsChart.kt
+++ src/jsMain/kotlin/com/occupantsearch/ui/AnalyticsChart.kt
@@ -1,7 +1,7 @@
 package com.occupantsearch.ui
 
 import com.occupantsearch.chartjs.Chart
-import csstype.Visibility
+import com.occupantsearch.chartjs.Chart.Companion.getChart
 import csstype.px
 import kotlinx.browser.document
 import kotlinx.coroutines.launch
@@ -10,51 +10,41 @@ import react.FC
 import react.Props
 import react.css.css
 import react.dom.html.ReactHTML.canvas
-import react.useEffectOnce
 import kotlin.js.Date
 
 external interface AnalyticsChartProps : Props {
-    var visible: Boolean
+    var id: String
 }
 
 val AnalyticsChart = FC<AnalyticsChartProps> { props ->
     canvas {
-        id = "canvas"
+        id = props.id
         css {
-            visibility = if (props.visible) {
-                Visibility.visible
-            } else {
-                Visibility.hidden
-            }
             marginTop = 60.px
         }
     }
-    if (props.visible) {
-        useEffectOnce {
-            scope.launch {
-                val analytics = client.getAnalytics()
-                val max = analytics.values.max()
+    if (getChart(props.id) == null) {
+        scope.launch {
+            val analytics = client.getAnalytics()
+            val max = analytics.values.max()
+            val canvas = document.getElementById(props.id) as HTMLCanvasElement
+            Chart(canvas, jso {
+                type = "bar"
+                data = jso {
+                    labels = analytics.keys
+                        .map { it.toDateString() }
+                        .toTypedArray()
+                    datasets = arrayOf(jso {
+                        backgroundColor = analytics.values.map { toBgColor(it, max) }.toTypedArray()
+                        borderColor = analytics.values.map { toColor(it, max) }.toTypedArray()
+                        label = "# of Posts"
+                        data = analytics.values.map { it.toInt() }.toTypedArray()
+                        borderWidth = 1
+                    })
+                }
+            })
 
-                val canvas = document.getElementById("canvas") as HTMLCanvasElement
-                Chart(canvas, jso {
-                    type = "bar"
-                    data = jso {
-                        labels = analytics.keys
-                            .map { it.toDateString() }
-                            .toTypedArray()
-                        datasets = arrayOf(jso {
-                            backgroundColor = analytics.values.map { toBgColor(it, max) }.toTypedArray()
-                            borderColor = analytics.values.map { toColor(it, max) }.toTypedArray()
-                            label = "# of Posts"
-                            data = analytics.values.map { it.toInt() }.toTypedArray()
-                            borderWidth = 1
-                        })
-                    }
-                })
-
-                canvas.style.display = "unset"
-                canvas.width = analytics.size * 20
-            }
+            canvas.style.display = "unset"
         }
     }
 }
@@ -67,6 +57,6 @@ fun toColor(value: Long, max: Long) = "hsl(${percent(value, max)}, 100%, 50%)"
 
 fun toBgColor(value: Long, max: Long) = "hsl(${percent(value, max)}, 100%, 50%, 0.2)"
 
-inline fun <T : Any> jso(): T = js("({})")
+fun <T : Any> jso(): T = js("({})")
 
 inline fun <T : Any> jso(builder: T.() -> Unit): T = jso<T>().apply(builder)
