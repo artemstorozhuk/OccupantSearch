746a6c86bd6edc8ee3c5621714b26d017a230203
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-01 20:21:22 +0100

    Fix react deprecation warning

--- src/jsMain/kotlin/com/occupantsearch/Main.kt
+++ src/jsMain/kotlin/com/occupantsearch/Main.kt
@@ -1,23 +1,15 @@
 package com.occupantsearch
 
 import com.occupantsearch.chartjs.init
-import com.occupantsearch.ui.PanelType
 import com.occupantsearch.ui.RootPanel
 import kotlinx.browser.document
 import react.create
-import react.dom.render
+import react.dom.client.createRoot
 
 fun main() {
     init()
-
-    val container = document.createElement("div")
-    document.body!!.appendChild(container)
-    render(RootPanel.create {
-        query = ""
-        occupants = listOf()
-        page = 0
-        lastPage = false
-        leftPanelOpen = false
-        visiblePanelType = PanelType.OCCUPANTS_LIST
-    }, container)
+    document.createElement("div").also {
+        document.body!!.appendChild(it)
+        createRoot(it).render(RootPanel.create {})
+    }
 }
