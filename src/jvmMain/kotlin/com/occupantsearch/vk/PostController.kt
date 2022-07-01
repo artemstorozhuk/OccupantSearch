5047261dc1539a201559bccb77b20fd7d9910283
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-01 20:33:21 +0100

    Cache latest post date

--- src/jvmMain/kotlin/com/occupantsearch/vk/PostController.kt
+++ src/jvmMain/kotlin/com/occupantsearch/vk/PostController.kt
@@ -5,16 +5,24 @@ import com.occupantsearch.time.secondsToMillis
 import com.vk.api.sdk.objects.wall.WallpostFull
 import org.koin.core.component.KoinComponent
 import java.util.Date
+import java.util.concurrent.atomic.AtomicReference
 
 class PostController(
     database: Database,
     private val vkNewsfeedSearcher: VkNewsfeedSearcher
 ) : KoinComponent {
     private val repository = database[WallpostFull::class.java]
+    private val latestPostDate = AtomicReference<Date>(calculateLatestPostDate())
 
-    fun getLatestPostDate() = repository.getAll().values.maxOfOrNull { it.date }?.secondsToMillis()?.let { Date(it) }
+    fun getLatestPostDate() = latestPostDate.get()
+
+    fun calculateLatestPostDate() = repository.getAll().values
+        .maxOfOrNull { it.date }
+        ?.secondsToMillis()
+        ?.let { Date(it) }
 
     fun downloadNewPosts() = vkNewsfeedSearcher.search(getLatestPostDate()) { posts ->
         repository.saveAll(posts.map { it.uniqueId to it })
+        latestPostDate.set(calculateLatestPostDate())
     }
 }
