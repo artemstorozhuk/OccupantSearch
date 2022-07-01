79c00d164d7c2191a3713be4dc3a31ebab5789a9
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-01 20:19:05 +0100

    Initialise chartjs at once

--- src/jsMain/kotlin/com/occupantsearch/chartjs/ChartInitializer.kt
+++ src/jsMain/kotlin/com/occupantsearch/chartjs/ChartInitializer.kt
@@ -1,28 +1,30 @@
 package com.occupantsearch.chartjs
 
-fun init() {
-    Chart.register(ArcElement)
-    Chart.register(LineElement)
-    Chart.register(BarElement)
-    Chart.register(PointElement)
-    Chart.register(BarController)
-    Chart.register(BubbleController)
-    Chart.register(DoughnutController)
-    Chart.register(LineController)
-    Chart.register(PieController)
-    Chart.register(PolarAreaController)
-    Chart.register(RadarController)
-    Chart.register(ScatterController)
-    Chart.register(CategoryScale)
-    Chart.register(LinearScale)
-    Chart.register(LogarithmicScale)
-    Chart.register(RadialLinearScale)
-    Chart.register(TimeScale)
-    Chart.register(TimeSeriesScale)
-    Chart.register(Decimation)
-    Chart.register(Filler)
-    Chart.register(Legend)
-    Chart.register(Title)
-    Chart.register(Tooltip)
-    Chart.register(SubTitle)
-}
+fun init() =
+    Chart.register(
+        ArcElement,
+        LineElement,
+        LineElement,
+        BarElement,
+        PointElement,
+        BarController,
+        BubbleController,
+        DoughnutController,
+        LineController,
+        PieController,
+        PolarAreaController,
+        RadarController,
+        ScatterController,
+        CategoryScale,
+        LinearScale,
+        LogarithmicScale,
+        RadialLinearScale,
+        TimeScale,
+        TimeSeriesScale,
+        Decimation,
+        Filler,
+        Legend,
+        Title,
+        Tooltip,
+        SubTitle,
+    )
