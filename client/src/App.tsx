2de625328da91319e57acc52cd9386f9f6e3e273
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-10 21:23:44 +0100

    Minor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnalyticsComponent } from './components/analytics/AnalyticsComponent';
-
 import { MenuDrawerOption } from './components/menu/MenuDrawer';
 import { OccupantComponent } from './components/occupant/OccupantComponent';
 import { OccupantSearchComponent } from './components/search/OccupantSearchComponent';
