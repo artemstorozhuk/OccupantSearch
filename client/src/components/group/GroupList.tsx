0f69ed91a4bc57a6c99f1d7895df6d7309b47fa4
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-16 22:54:11 +0100

    Add infinite scroll for group list

--- client/src/components/group/GroupList.tsx
+++ client/src/components/group/GroupList.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from "react";
+import { Component, createRef, ReactNode } from "react";
 import { getGroups } from "../../client/Client";
 import Group from "../../model/Group";
 import GroupCard from "./GroupCard";
@@ -7,6 +7,7 @@ export interface GroupListState {
     page: number,
     loading: boolean,
     groups: Array<Group>,
+    hasMore: boolean
 }
 
 export default class GroupList extends Component<{}, GroupListState> {
@@ -14,16 +15,34 @@ export default class GroupList extends Component<{}, GroupListState> {
         page: 0,
         loading: false,
         groups: [],
+        hasMore: true,
+    }
+
+    private root = createRef<HTMLDivElement>()
+
+    private onScroll = () => {
+        if (!this.state.loading &&
+            this.state.hasMore &&
+            this.root?.current != null &&
+            window.scrollY + 4 * window.innerHeight > this.root.current.clientHeight) {
+            this.load(this.state.page, this.state.groups)
+        }
     }
 
     componentDidMount() {
         this.load(0, [])
+        document.addEventListener('scroll', this.onScroll)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.onScroll)
     }
 
     render(): ReactNode {
-        return (
-            this.state.groups.map((group, index) => <GroupCard group={group} key={index} />)
-        )
+        return <div
+            ref={this.root}>
+            {this.state.groups.map((group, index) => <GroupCard group={group} key={index} />)}
+        </div>
     }
 
     load(page: number, groups: Array<Group>) {
@@ -33,6 +52,7 @@ export default class GroupList extends Component<{}, GroupListState> {
                 loading: false,
                 page: page + 1,
                 groups: groups.concat(result),
+                hasMore: result.length > 0
             })
         })
     }
