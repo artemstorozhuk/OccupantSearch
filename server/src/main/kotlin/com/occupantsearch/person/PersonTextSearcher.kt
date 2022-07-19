ad9ee89fd0acc9f994fa27e4de8e79bbf8df3c85
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-19 23:31:40 +0100

    Fix person searcher

--- server/src/main/kotlin/com/occupantsearch/person/PersonTextSearcher.kt
+++ server/src/main/kotlin/com/occupantsearch/person/PersonTextSearcher.kt
@@ -12,25 +12,27 @@ class PersonTextSearcher(
     private val names = resourceReader.readResourceAsSet("/names/firstnames.txt")
     private val surnames = resourceReader.readResourceAsSet("/names/lastnames.txt")
 
-    fun search(text: String) =
-        textBlockSplitter.splitByBlocks(text)
-            .map { block ->
-                val words = block.split(" ")
-                    .filter { it.isNotBlank() }
-                words.mapIndexedNotNull { index, word ->
-                    if (word in names) {
-                        if (index < words.size - 1 && words[index + 1] in surnames) {
-                            Person(word, words[index + 1])
-                        } else if (index > 0 && words[index - 1] in surnames) {
-                            Person(word, words[index - 1])
-                        } else {
-                            null
-                        }
-                    } else {
-                        null
+    fun search(text: String): Set<Person> {
+        val persons = HashSet<Person>()
+        textBlockSplitter.splitByBlocks(text).forEach { block ->
+            val words = block.split(" ")
+                .filter { it.isNotBlank() }
+            var index = 0
+            while (index < words.size - 1) {
+                val word = words[index]
+                when {
+                    word in names && words[index + 1] in surnames -> {
+                        persons.add(Person(word, words[index + 1]))
+                        index += 2
                     }
+                    word in surnames  && words[index + 1] in names -> {
+                        persons.add(Person(words[index + 1], word))
+                        index += 2
+                    }
+                    else -> index++
                 }
             }
-            .flatten()
-            .toSet()
+        }
+        return persons
+    }
 }
