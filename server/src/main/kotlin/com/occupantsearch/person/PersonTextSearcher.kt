d818c65e7087f5164629aa8eea161b41b812a1a3
Artem Storozhuk <artemstorozhuk@gmail.com>
2022-07-20 22:42:12 +0100

    Replace foreach with fold

--- server/src/main/kotlin/com/occupantsearch/person/PersonTextSearcher.kt
+++ server/src/main/kotlin/com/occupantsearch/person/PersonTextSearcher.kt
@@ -12,27 +12,28 @@ class PersonTextSearcher(
     private val names = resourceReader.readResourceAsSet("/names/firstnames.txt")
     private val surnames = resourceReader.readResourceAsSet("/names/lastnames.txt")
 
-    fun search(text: String): Set<Person> {
-        val persons = HashSet<Person>()
-        textBlockSplitter.splitByBlocks(text).forEach { block ->
-            val words = block.split(" ")
-                .filter { it.isNotBlank() }
-            var index = 0
-            while (index < words.size - 1) {
-                val word = words[index]
-                when {
-                    word in names && words[index + 1] in surnames -> {
-                        persons.add(Person(word, words[index + 1]))
-                        index += 2
+    fun search(text: String) =
+        textBlockSplitter.splitByBlocks(text)
+            .asSequence()
+            .map { block ->
+                val words = block.split(" ")
+                    .filter { it.isNotBlank() }
+                words.foldIndexed(PersonSearch(setOf(), 0)) { index, acc, word ->
+                    when {
+                        index == words.size - 1 || index < acc.index -> acc
+                        word in names && words[index + 1] in surnames ->
+                            PersonSearch(acc.persons + Person(word, words[index + 1]), index + 2)
+                        word in surnames && words[index + 1] in names ->
+                            PersonSearch(acc.persons + Person(words[index + 1], word), index + 2)
+                        else -> acc
                     }
-                    word in surnames  && words[index + 1] in names -> {
-                        persons.add(Person(words[index + 1], word))
-                        index += 2
-                    }
-                    else -> index++
-                }
+                }.persons
             }
-        }
-        return persons
-    }
+            .flatten()
+            .toSet()
+
+    data class PersonSearch(
+        val persons: Set<Person>,
+        val index: Int
+    )
 }
